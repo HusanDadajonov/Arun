3ec9817a9dc1e80d82047fe5d2cd4dd7d0f0c38c
am1rcek <xusandadajonov4385@gmail.com>
2021-09-16 11:07:52 -0700

    the hend

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let elDark = document.querySelector(".sun-btn");
 let body = document.querySelector('body');
-
+let elSwitch =document.querySelector(".switch-img");
+let elMainswitch = document.querySelector(".header-switch");
+let elcheck = document.querySelector(".check");
+let elheader = document.querySelector("header");
+elcheck .addEventListener("click", function() {
+   elheader.classList.toggle("fixed-header");
+})
+elSwitch.addEventListener("click", function() {
+   elMainswitch.classList.toggle("cahnge-switch");
+})
 elDark.addEventListener("click", function() {
    body.classList.toggle("change-color");
 });
